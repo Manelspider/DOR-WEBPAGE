92363ae1423e656f8b007e721e9eafa749d52f93
Manelspider <manelspider1b30@gmail.com>
2023-10-24 03:18:43 +0100

    Finalizado del prototipo de DOR

---
+++ Scripts/add_default_content_profile.js
@@ -0,0 +1,6 @@
+document.querySelector('input[name="p-name"]').value = 'Manuel';
+document.querySelector('input[name="p-surr"]').value = 'González';
+document.querySelector('input[name="p-mail"]').value = 'prueba123@gmail.com';
+document.querySelector('input[name="p-tlf"]').value = '922583020';
+document.querySelector('input[name="p-locat"]').value = 'Santa Cruz de Tenerife';
+document.querySelector('input[name="p-passwd"]').value = 'Santa Cruz de Tenerife';
